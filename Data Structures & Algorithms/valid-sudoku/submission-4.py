7bb17b32de2aaf28e460807a57c7a48719350692
Jenith Mehta <71172711+jenithmehta@users.noreply.github.com>
2026-06-18 01:29:04 +0530

    Add: valid-sudoku - submission-4

---
+++ Data Structures & Algorithms/valid-sudoku/submission-4.py
@@ -0,0 +1,19 @@
+class Solution:
+    def isValidSudoku(self, board: list[list[str]]) -> bool:
+        # lets create a hashset
+        rows = collections.defaultdict(set)
+        cols = collections.defaultdict(set)
+        squares = collections.defaultdict(set) # here the key will be ratios of r/3,c/3
+
+        for r in range(0,9):
+            for c in range(0,9):
+                if board[r][c] == ".":
+                    continue
+                if (board[r][c] in rows[r]) or (
+                    board[r][c] in cols[c]) or (
+                        board[r][c] in squares[r//3,c//3]):
+                    return False
+                rows[r].add(board[r][c])
+                cols[c].add(board[r][c])
+                squares[r//3,c//3].add(board[r][c])
+        return True
